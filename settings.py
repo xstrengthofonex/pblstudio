463fc3e37bd8716515b485fc525c3ff804f14785
Vincent White <xstrengthofonex@gmail.com>
2019-07-21 19:54:20 +0900

    Fixed DB URI

--- settings.py
+++ settings.py
@@ -5,3 +5,4 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_DIR = os.path.join(ROOT_DIR, "static")
 TEMPLATES_DIR = os.path.join(ROOT_DIR, "templates")
 DB_URI = os.getenv("MONGODB_URI", "mongodb://localhost:27017")
+DB_NAME = "heroku_q9nc1d4v"
